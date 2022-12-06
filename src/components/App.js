09cddc424d5dd349af5ff3d88ba82eb6036d8f33
Arun Singh Pundir <Arunsinghpundir.325@gmail.com>
2022-12-06 15:14:32 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,6 @@ import React from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
-const App = () => {
-  return (
-    <Welcome name="Arun">Welcome to Newton School.</Welcome>
-  )
-}
-
+const App = () => {  return (    <Welcome name="" />  )}
 
 export default App;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,3 @@
-// write code for Welcome component here
-import React from "react";
- const Welcome = (student)=>{
-    return (
-    <div>
-        <h1>Hey !{student.name}</h1>
-        <h2>{student.children}</h2>
-    </div>
-    )
-}
-export default Welcome;
+import React from 'react'
+const Welcome = (props) => {  return (    <div>        <h1>Hey {props.name}!</h1>        <h2>Welcome to Newton School.</h2>    </div>  )}
+export default Welcome
